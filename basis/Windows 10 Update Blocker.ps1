bf1d8f9f9800cc41feadf91e4b7125d78d5e3334
distilledliquor <54929046+distilledliquor@users.noreply.github.com>
2021-04-06 20:57:55 +0900

    Update Windows 10 Update Blocker.ps1

--- basis/Windows 10 Update Blocker.ps1
+++ basis/Windows 10 Update Blocker.ps1
@@ -1,10 +1,10 @@
 <# Setting execution policy into Bypass for this PSSession only (local machine is not be affected) #>
-Set-ExecutionPolicy -Force Bypass -Scope Process
+Set-ExecutionPolicy Bypass Process
 
 <# Setting registry values to stop windows update #>
-$key = "Registry::HKLM\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings"
-$valTimeMin = "0000-01-01T00:00:00Z"
-$valTimeMax = "9999-09-09T00:00:00Z"
+$key = 'Registry::HKLM\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings'
+$valTimeMin = '0000-01-01T00:00:00Z'
+$valTimeMax = '9999-09-09T00:00:00Z'
 New-ItemProperty -Force -Path $key -PropertyType String -Name PauseFeatureUpdatesStartTime -Value $valTimeMin
 New-ItemProperty -Force -Path $key -PropertyType String -Name PauseQualityUpdatesStartTime -Value $valTimeMin
 New-ItemProperty -Force -Path $key -PropertyType String -Name PauseFeatureUpdatesEndTime -Value $valTimeMax
@@ -14,9 +14,9 @@ New-ItemProperty -Force -Path $key -PropertyType String -Name PauseUpdatesExpiry
 <# Completion message #>
 [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
 [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] > $null
-[string] $L1 = "Windows 업데이트가 9999년 9월 9일에 다시 시작됩니다."
-[string] $L2 = "　"
-[string] $L3 = "Windows 업데이트 창의 업데이트 계속하기 버튼을 클릭하여 Windows 업데이트 기능을 복구하실 수 있습니다."
+[string] $L1 = 'Windows 업데이트가 9999년 9월 9일에 다시 시작됩니다.'
+[string] $L2 = '　'
+[string] $L3 = 'Windows 업데이트 창의 업데이트 계속하기 버튼을 클릭하여 Windows 업데이트 기능을 복구하실 수 있습니다.'
 $toastDetail = @"
 <toast> 
     <visual>
@@ -31,5 +31,5 @@ $toastDetail = @"
 $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
 $xml.LoadXml($toastDetail)
 $toastNotification = [Windows.UI.Notifications.ToastNotification]::new($xml)
-[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier(" ").Show($toastNotification)
+[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier(' ').Show($toastNotification)
 
